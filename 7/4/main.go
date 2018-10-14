123ec585a451c713e32da7f58de4eb4bcd55c134
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-14 22:49:10 +0900

    テンプレートの処理/ネストしたフィールドの内容の出力

--- 7/4/main.go
+++ 7/4/main.go
@@ -5,14 +5,35 @@ import (
 	"os"
 )
 
+type Friend struct {
+	Fname string
+}
+
 type Person struct {
 	UserName string
-	email    string
+	Emails   []string
+	Friends  []*Friend
 }
 
 func main() {
+	f1 := Friend{Fname: "minux.ma"}
+	f2 := Friend{Fname: "xushiwei"}
 	t := template.New("fieldname exapmle")
-	t, _ = t.Parse("hello {{.UserName}}! {{.email}}")
-	p := Person{UserName: "Astaxie", email: "test@test.com"}
+	t, _ = t.Parse(`
+    hello {{.UserName}}!
+    {{range .Emails}}
+      an email {{.}}
+    {{end}}
+    {{with .Friends}}
+    {{range .}}
+      my friend name is {{.Fanme}}
+    {{end}}
+    {{end}}
+  `)
+	p := Person{
+		UserName: "Astaxie",
+		Emails:   []string{"astaxie@beego.me", "astaxie@gmail.com"},
+		Friends:  []*Friend{&f1, &f2},
+	}
 	t.Execute(os.Stdout, p)
 }
