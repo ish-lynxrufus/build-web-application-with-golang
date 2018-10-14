50ae7d0197fd76a1c3cd458ff7bc6658a5884411
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-14 23:40:51 +0900

    ファイルの操作/ファイルへの書き込み

--- 7/5/main.go
+++ 7/5/main.go
@@ -8,9 +8,21 @@ import (
 func main() {
 	os.Mkdir("test", 0777)
 	os.MkdirAll("subdir/sub1/sub2", 0777)
-	err := os.Remove("dummy")
+	err := os.Remove("test")
 	if err != nil {
 		fmt.Println(err)
 	}
 	os.RemoveAll("subdir")
+
+	userFile := "test.txt"
+	fout, err := os.Create(userFile)
+	if err != nil {
+		fmt.Println(userFile, err)
+		return
+	}
+	defer fout.Close()
+	for i := 0; i < 10; i++ {
+		fout.WriteString("これはテストです。\r\n")
+		fout.Write([]byte("これはテストです。\r\n"))
+	}
 }
