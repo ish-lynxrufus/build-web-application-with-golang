0be460432088112d6d9e5223824a8b75230e0cb1
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-14 23:45:57 +0900

    ファイルの操作/ファイルの読み込み

--- 7/5/main.go
+++ 7/5/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// ディレクトリの操作
 	os.Mkdir("test", 0777)
 	os.MkdirAll("subdir/sub1/sub2", 0777)
 	err := os.Remove("test")
@@ -14,6 +15,7 @@ func main() {
 	}
 	os.RemoveAll("subdir")
 
+	// ファイルへの書き込み
 	userFile := "test.txt"
 	fout, err := os.Create(userFile)
 	if err != nil {
@@ -24,5 +26,21 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fout.WriteString("これはテストです。\r\n")
 		fout.Write([]byte("これはテストです。\r\n"))
+	}
+
+	// ファイルの読み込み
+	fl, err := os.Open(userFile)
+	if err != nil {
+		fmt.Println(userFile, err)
+		return
+	}
+	defer fl.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, _ := fl.Read(buf)
+		if 0 == n {
+			break
+		}
+		os.Stdout.Write(buf[:n])
 	}
 }
